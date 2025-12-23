a297935f28d95613137b6cce8cecf6dc0bbf0609
sapir <sapirlev@my.hit.ac.il>
2025-12-23 23:49:55 +0200

    sapir2

--- app/src/main/java/com/example/myapplicationsapir/DetailItemFragment.kt
+++ app/src/main/java/com/example/myapplicationsapir/DetailItemFragment.kt
@@ -5,14 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplicationsapir.databinding.DetailItemLayoutBinding
 
 class DetailItemFragment : Fragment() {
+
+    var _binding : DetailItemLayoutBinding? = null
+
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding = DetailItemLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
